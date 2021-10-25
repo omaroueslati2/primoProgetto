a7687be7984dab4edd74039314637baab3caacc9
omar oueslati <omar.oueslati2@soprasteria.com>
2021-10-25 17:26:56 +0200

    aguinta del drop down

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
+window.addEventListener("resize",function(){
+    media()
+})
 
+function media(){
+let media =window.matchMedia("(max-width: 995px)")
+let elem = document.getElementsByClassName("navbar-nav")[0]
+if (media.matches) {
+    
+   
+   let li = document.createElement("li")
+   if (elem.childElementCount == 6) {
+    li.innerHTML='<li class="nav-item"><a class="nav-link" href="#">Store Locator</a></li> ' 
+    +'<li class="nav-item"><a class="nav-link" href="#">italia|ita</a></li>' +
+    '<li class="nav-item"><a class="nav-link" href="#">Area Account</a></li>'+
+    '<li class="nav-item"><a class="nav-link" href="#">servizio clienti</a></li>'+
+    '<li class="nav-item"><a class="nav-link" href="#">AREA LEGALE</a></li>'
+    elem.append(li)
+   }
+   
+   
+}
+else{
+    while(elem.childElementCount != 6){
+        elem.removeChild(elem.lastChild)
+    }
+}
 
+}
