a69a467ed9f03e54061229bcf036b240d75e3117
omar oueslati <omar.oueslati2@soprasteria.com>
2021-10-26 17:23:33 +0200

    realizazione del 2 carosello

--- js/script.js
+++ js/script.js
@@ -1,33 +1,47 @@
+$(".carosseloResp").slick({
+  centerMode: true,
+  centerPadding: "60px",
+  slidesToShow: 1,
+
+  prevArrow: '<i class="fas fa-chevron-circle-left"></i>',
+  nextArrow: '<i class="fas fa-chevron-circle-right"></i>',
+  responsive: [
+    {
+      breakpoint: 995,
+      settings: {},
+    },
+  ],
+});
+
+$(".carosello1").slick({
+  prevArrow: '<i class="fas fa-chevron-left"></i>',
+  nextArrow: '<i class="fas fa-chevron-right"></i>',
+});
+
+window.addEventListener("resize", function () {
+  
+  media();
+});
 
 
-  $('.carosello1').slick();
+function media() {
+  let media = window.matchMedia("(max-width: 995px)");
+  let elem = document.getElementsByClassName("navbar-nav")[0];
 
-window.addEventListener("resize",function(){
-    media()
-})
-
-function media(){
-let media =window.matchMedia("(max-width: 995px)")
-let elem = document.getElementsByClassName("navbar-nav")[0]
-if (media.matches) {
-    
-   
-   let li = document.createElement("li")
-   if (elem.childElementCount == 6) {
-    li.innerHTML='<li class="nav-item"><a class="nav-link" href="#">Store Locator</a></li> ' 
-    +'<li class="nav-item"><a class="nav-link" href="#">italia|ita</a></li>' +
-    '<li class="nav-item"><a class="nav-link" href="#">Area Account</a></li>'+
-    '<li class="nav-item"><a class="nav-link" href="#">servizio clienti</a></li>'+
-    '<li class="nav-item"><a class="nav-link" href="#">AREA LEGALE</a></li>'
-    elem.append(li)
-   }
-   
-   
-}
-else{
-    while(elem.childElementCount != 6){
-        elem.removeChild(elem.lastChild)
+  if (media.matches) {
+    let li = document.createElement("li");
+    if (elem.childElementCount == 6) {
+      li.innerHTML =
+        '<li class="nav-item"><a class="nav-link" href="#">Store Locator</a></li> ' +
+        '<li class="nav-item"><a class="nav-link" href="#">italia|ita</a></li>' +
+        '<li class="nav-item"><a class="nav-link" href="#">Area Account</a></li>' +
+        '<li class="nav-item"><a class="nav-link" href="#">servizio clienti</a></li>' +
+        '<li class="nav-item"><a class="nav-link" href="#">AREA LEGALE</a></li>';
+      elem.append(li);
     }
-}
-
+  } else {
+    while (elem.childElementCount != 6) {
+      elem.removeChild(elem.lastChild);
+    }
+  }
 }
