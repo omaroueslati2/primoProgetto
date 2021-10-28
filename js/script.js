8d105df143ebe43ad4d6b70c4e59e5ead23b16d4
omar oueslati <omar.oueslati2@soprasteria.com>
2021-10-28 17:43:39 +0200

    agguinta d del menu humburguer

--- js/script.js
+++ js/script.js
@@ -5,43 +5,41 @@ $(".carosseloResp").slick({
 
   prevArrow: '<i class="fas fa-chevron-circle-left"></i>',
   nextArrow: '<i class="fas fa-chevron-circle-right"></i>',
-  responsive: [
-    {
-      breakpoint: 995,
-      settings: {},
-    },
-  ],
+ 
 });
 
 $(".carosello1").slick({
   prevArrow: '<i class="fas fa-chevron-left"></i>',
   nextArrow: '<i class="fas fa-chevron-right"></i>',
 });
-
-window.addEventListener("resize", function () {
+var i = 1 ;
+var elem = $("span")[0]
+var elem1 = $("span")[1]
+var elem2 = $("span")[2]
+$(".pulsante").click(function(){
+$(elem).toggleClass("primaS")
+$(elem1).toggleClass("secondaS")
+$(elem2).toggleClass("terzaT")
   
-  media();
-});
+ /* if ($(elem1).hasClass("sconda")) {
+   add("primaS","secondaS","terzaT")
+   remuve("prima","sconda","terza")
+   
+ }
+ else if($(elem1).hasClass("secondaS")){
+   add("prima","seconda","terza")
+   remuve("primaS","secondaS","terzaT") */
+ 
+  
+})
+function add( pram,param1,param2){
+  $(elem).addClass(pram)
+  $(elem1).addClass(param1)
+  $(elem2).addClass(param2)
 
-
-function media() {
-  let media = window.matchMedia("(max-width: 995px)");
-  let elem = document.getElementsByClassName("navbar-nav")[0];
-
-  if (media.matches) {
-    let li = document.createElement("li");
-    if (elem.childElementCount == 6) {
-      li.innerHTML =
-        '<li class="nav-item"><a class="nav-link" href="#">Store Locator</a></li> ' +
-        '<li class="nav-item"><a class="nav-link" href="#">italia|ita</a></li>' +
-        '<li class="nav-item"><a class="nav-link" href="#">Area Account</a></li>' +
-        '<li class="nav-item"><a class="nav-link" href="#">servizio clienti</a></li>' +
-        '<li class="nav-item"><a class="nav-link" href="#">AREA LEGALE</a></li>';
-      elem.append(li);
-    }
-  } else {
-    while (elem.childElementCount != 6) {
-      elem.removeChild(elem.lastChild);
-    }
-  }
+}
+function remuve(pram,param1,param2){
+  $(elem).removeClass(pram)
+  $(elem1).removeClass(param1)
+  $(elem2).removeClass(param2)
 }
