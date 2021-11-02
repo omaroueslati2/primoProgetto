3dcb8799280a13df9b61e2601206c2b395bc7329
omar oueslati <omar.oueslati2@soprasteria.com>
2021-11-02 10:36:46 +0100

    agguinta del plug in jquary al proggetto creare l'annimazione per il menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+/// <reference path="../typings/globals/jquery/index.d.ts" />
+
+
 $(".carosseloResp").slick({
   centerMode: true,
   centerPadding: "60px",
@@ -18,7 +21,8 @@ var elem1 = $("span")[1]
 var elem2 = $("span")[2]
 var i = 1 
 $(".pulsante").click(function(){
-  console.log(i)
+
+  console.log()
 $(elem).toggleClass("primaS")
 $(elem1).toggleClass("secondaS")
 $(elem2).toggleClass("terzaT")
@@ -26,15 +30,24 @@ if(i%2!=0){$(".menuLaterale").animate({left:"0vw"},1000)}
 else{
   $(".menuLaterale").animate({left:"-100vw"},1000)
 }
-  i++
- /* if ($(elem1).hasClass("sconda")) {
-   add("primaS","secondaS","terzaT")
-   remuve("prima","sconda","terza")
-   
- }
- else if($(elem1).hasClass("secondaS")){
-   add("prima","seconda","terza")
-   remuve("primaS","secondaS","terzaT") */
- 
+  i++ 
+}) /*fine del click */
+
+$(window).on('scroll',function(){
+    console.log("omarrrrrr")
+    console.log($(".spedizione").height())
+    console.log( $("html,body").scrollTop().valueOf()>= $(".spedizione"))
+    console.log($("html,body").scrollTop().valueOf())
+    if( $("html,body").scrollTop().valueOf()> $(".spedizione").height()){
+      $(".header").stop().animate({top:"0vh"},500)
+    }
+    else{
+      $(".header").stop().animate({top:"4vh"},500)
+    }
+  }
+)
   
-})
+
+  
+
+
