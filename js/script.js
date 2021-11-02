255f54f6e1b35aa4f2ffeff6666e3397d330e34e
omar oueslati <omar.oueslati2@soprasteria.com>
2021-11-02 17:57:30 +0100

    cambio de css

--- js/script.js
+++ js/script.js
@@ -1,44 +1,69 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 
-$(".carosseloResp").slick({
+$(".secondoCarosselo").slick({
   centerMode: true,
   centerPadding: "60px",
   slidesToShow: 1,
 
   prevArrow: '<i class="fas fa-chevron-circle-left"></i>',
   nextArrow: '<i class="fas fa-chevron-circle-right"></i>',
+  responsive: [
+    {
+        breakpoint: 995,
+        settings: { 
+          centerMode: false,
+          
+         }
+    },
+    ]
 });
 
-$(".carosello1").slick({
+$(".heroBanner").slick({
   prevArrow: '<i class="fas fa-chevron-left"></i>',
   nextArrow: '<i class="fas fa-chevron-right"></i>',
+  
 });
-var i = 1;
-var elem = $("span")[0];
-var elem1 = $("span")[1];
-var elem2 = $("span")[2];
-var i = 1;
-$(".pulsante").click(function () {
-  console.log();
-  $(elem).toggleClass("primaS");
-  $(elem1).toggleClass("secondaS");
-  $(elem2).toggleClass("terzaT");
-  if (i % 2 != 0) {
-    $(".menuLaterale").animate({ left: "0vw" }, 1000);
-  } else {
-    $(".menuLaterale").animate({ left: "-100vw" }, 1000);
+
+$('.carosello3').slick();
+
+
+var elem = $(".header_menu__humburger")[0];
+var i = 0
+$(elem).on("click",function () {
+  console.log(this)
+  $(this).toggleClass("open")
+  console.log(i%2 == 0)
+  if (i%2 == 0) {
+    $(".header_menu__mobile").stop().animate({left:"0vw" },1000)
   }
-  i++;
-}); /*fine del click */
+  else{
+    $(".header_menu__mobile").stop().animate({left:"-100vw" },1000)
+  }
+  i++
+})
 
 $(window).on("scroll", function () {
-  console.log("omarrrrrr");
-  console.log($(".spedizione").height());
-  console.log($("html,body").scrollTop().valueOf() >= $(".spedizione"));
-  console.log($("html,body").scrollTop().valueOf());
-  if ($("html,body").scrollTop().valueOf() > $(".spedizione").height()) {
-    $(".header").stop().animate({ top: "0vh" }, 500);
+ console.log("omar")
+  if ($("html,body").scrollTop().valueOf() > $(".header_sidebar").height()) {
+    $(".header_menu").stop().animate({ top: "0vh" }, 500);
   } else {
-    $(".header").stop().animate({ top: "4vh" }, 500);
+    $(".header_menu").stop().animate({ top: "4vh" }, 500);
   }
 });
+
+/*$(window).on("resize", function () {
+  console.log($(window).width()<=995)
+  console.log($(window).width())
+  if ($(window).width() < 995 ) {
+    $('.carosseloResp').not('.slick-initialized').slick(
+      {
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    );
+  }
+  else{
+    
+  }
+})*/
