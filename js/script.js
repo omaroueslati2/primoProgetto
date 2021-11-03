b10ab051749b654b8ddb780cd21112334029c483
omar oueslati <omar.oueslati2@soprasteria.com>
2021-11-03 16:56:56 +0100

    aguinta del terzo carozello

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
+$(".carosseloCollezione").slick({
+  prevArrow: '<i class="fas fa-chevron-left"></i>',
+  nextArrow: '<i class="fas fa-chevron-right"></i>',
+}
+ 
+);
 
 $(".secondoCarosselo").slick({
   centerMode: true,
