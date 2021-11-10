bdc8eebcc573e4f0d66ede6218f04677322d44f0
omar oueslati <omar.oueslati2@soprasteria.com>
2021-11-10 17:40:33 +0100

    agguinta dela modale

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 
-var child = $(".header_menu__centrale").children()
-console.log(child)
+var child = $(".header_menu__centrale").children();
 
-$(child[0]).on({
-  mouseenter: function () {
-    $($(".menu_nascosto")[0]).toggleClass("nuoviArrivi_menuINvisibile")
-    $($(".menu_nascosto")[0]).toggleClass("nuoviarrivi_menu")  },
-  mouseleave: function () {
-    $($(".menu_nascosto")[0]).toggleClass("nuoviArrivi_menuINvisibile")
-    $($(".menu_nascosto")[0]).toggleClass("nuoviarrivi_menu")  }
-})
-
+child.each(function (index) {
+  $(this).on({
+    mouseenter: function () {
+      if (index != 3) {
+        $($(".menu_nascosto")[index]).toggleClass("nuoviArrivi_menuINvisibile");
+        $($(".menu_nascosto")[index]).toggleClass("nuoviarrivi_menu");
+      }
+    },
+    mouseleave: function () {
+      if (index != 3) {
+        $($(".menu_nascosto")[index]).toggleClass("nuoviArrivi_menuINvisibile");
+        $($(".menu_nascosto")[index]).toggleClass("nuoviarrivi_menu");
+      }
+    },
+  });
+});
 
 var elem = $("video")[0]; /*play video*/
-console.log(elem);
 $(elem).on("click", function () {
-  console.log("difla");
   if (this.paused) {
     this.play();
   } else {
@@ -77,20 +81,43 @@ var elem = $(".header_menu__humburger")[0];
 var i = 0;
 $(elem).on("click", function () {
   $(this).toggleClass("open");
-  console.log($(window).width());
   if (i % 2 == 0 && $(window).width() < 900) {
     $(".header_menu__mobile").stop().animate({ left: "0vw" }, 1000);
-    
+    $(".header_menu__mobile li").each(function (index) {
+      $(this).on("click", function () {
+        console.log(index == 5);
+        if (index == 5) {
+          $(location).attr(
+            "href",
+            "https://www.harmontblaine.com/IT/it/storelocator"
+          );
+        } else {
+          $($(".header_stotomenu__mobile")[index])
+            .stop()
+            .animate({ left: "0vw" }, 1000);
+          $(".indietro").each(function () {
+            $(this).on("click", function () {
+              $($(".header_stotomenu__mobile")[index])
+                .stop()
+                .animate({ left: "-100vw" }, 1000);
+            });
+          });
+        }
+      });
+    });
     $("html,body").css({
-      "height": '100%',
-      "overflow-y": "hidden"
-    })
+      height: "100%",
+      "overflow-y": "hidden",
+    });
   } else {
     $(".header_menu__mobile").stop().animate({ left: "-100vw" }, 1000);
+
+    $(".header_stotomenu__mobile").stop().animate({ left: "-100vw" }, 1000);
+
     $("html,body").css({
-      "height": "auto",
-      "overflow-y": "auto"
-    })
+      height: "auto",
+      "overflow-y": "auto",
+    });
   }
   i++;
 });
@@ -98,18 +125,22 @@ $(elem).on("click", function () {
 $(window).on("resize", function () {
   if ($(window).width() > 995) {
     $("html,body").css({
-      "height": "auto",
-      "overflow-y": "auto"
-    })
+      height: "auto",
+      "overflow-y": "auto",
+    });
     $(".header_menu__mobile").stop().animate({ left: "-100vw" }, 1000);
   }
 });
 $(window).on("scroll", function (e) {
-  
   if ($("html,body").scrollTop().valueOf() > $(".header_sidebar").height()) {
-    $(".header_menu").stop().animate({ top: "0vh" }, 500);
+    $(".header_menu").stop().animate({ top: "0vh" }, 1500);
+    $(".menu_nascosto").each(function () {
+      $(this).stop().animate({ top: "6vh" });
+    });
   } else {
-    $(".header_menu").stop().animate({ top: "4vh" }, 500);
+    $(".header_menu").stop().animate({ top: "4vh" }, 1500);
+    $(".menu_nascosto").each(function () {
+      $(this).stop().animate({ top: "12vh" });
+    });
   }
 });
-
